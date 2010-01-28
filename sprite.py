9bf72c95b8263aae2ada31d2a01d39da01c6ed57
Michael Simpson <michael@abstract.fm>
2010-01-28 05:10:48 +0000

    Added hp variable to all sprites. They now have vitality.

--- sprite.py
+++ sprite.py
@@ -4,11 +4,12 @@ from pygame.sprite import DirtySprite
 class Sprite(DirtySprite):
     """The base sprite class that all sprites inherit."""
 
-    def __init__(self, size, color=(0,0,0)):
+    def __init__(self, size, starting_hp, color=(0,0,0)):
         DirtySprite.__init__(self)
         self.image = Surface(size).convert()
         self.image.fill(color)
         self.rect = self.image.get_rect()
+        self.hp = starting_hp
 
     def collide(self, scene):
         """Check if the sprite collided with an object or screen edge."""
